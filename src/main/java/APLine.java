a91fcaa9cb6e9f94deaf5a73bfe47f6dd12fa51c
Sebastian Dowell <142616573+SebastianAPCS@users.noreply.github.com>
2024-04-20 17:42:32 +0000

    write APLine class

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,17 @@
-public class APLine
-{
-  /* your code here */
-  
+public class APLine {
+  int a, b, c;
+
+  public APLine(int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  public double getSlope() {
+    return -a / b;
+  }
+
+  public boolean isOnLine(double x, double y) {
+    return (this.a * x + this.b * y + c) == 0;
+  }
 }
